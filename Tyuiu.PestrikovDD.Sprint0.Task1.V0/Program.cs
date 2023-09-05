ff5d860feb4c3c01c9122a5c3068643b76893e82
Данила Пестриков <theuselessfox1@gmail.com>
2023-09-05 14:05:34 +0300

    Task1

---
+++ Tyuiu.PestrikovDD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PestrikovDD.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Игорь";
+            int age = 45;
+            Console.WriteLine("Меня зовут " + name + "мне" + age + "лет!");
+            Console.ReadKey();
+        }
+    }
+}
