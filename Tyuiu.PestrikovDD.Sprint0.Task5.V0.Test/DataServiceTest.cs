99cebdbcad0c40430020958269d3644e86c76a76
Данила Пестриков <theuselessfox1@gmail.com>
2023-09-14 21:48:35 +0300

    Task5

---
+++ Tyuiu.PestrikovDD.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PestrikovDD.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.PestrikovDD.Sprint0.Task5.V0.Test
+{
+   [TestClass]
+   public class DataServiceTest
+   {
+    [TestMethod]
+    public void CheckedAdditionValid()
+    {
+        Assert.AreEqual(10, DataService.Addition(5, 5));
+    }
+
+    [TestMethod]
+    public void CheckedSubstrustionValid()
+    {
+        Assert.AreEqual(5, DataService.Substruction(10, 5));
+    }
+
+    [TestMethod]
+    public void CheckedMultiplicationValid()
+    {
+        Assert.AreEqual(50, DataService.Multiplication(10, 5));
+    }
+
+    [TestMethod]
+    public void CheckedDivisionValid()
+    {
+        Assert.AreEqual(3, DataService.Division(9, 3));
+    }
+}
+}
---
+++ Tyuiu.PestrikovDD.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PestrikovDD.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PestrikovDD.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db517cb8-a1d5-47b0-b421-e2092471daa9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
